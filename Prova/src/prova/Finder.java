c8990a6564becd91631d6c7d339df6b22bf9bcb5
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-04-03 23:40:25 -0300

    fixed finder method

--- Prova/src/prova/Finder.java
+++ Prova/src/prova/Finder.java
@@ -11,106 +11,76 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Finder extends Thread {
-	String name;
 	String type;
-	String date;
+	String name;
 	int size;
-	String content;
 	String path;
 	Thread atual = Thread.currentThread();
 
-	public Finder(
-			String name, 
-			String type, 
-			String date, 
-			int size, 
-			String content, 
-			String path) throws IOException 
-	{
-		this.name = name;
+	@Override
+	public void run() {
+		try {
+			findFile(this.type, this.name, this.size, this.path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Finder(String type, String name, int size, String path) throws IOException {
 		this.type = type;
-		this.date = date;
+		this.name = name;
 		this.size = size;
-		this.content = content;
 		this.path = path;
 
 		start();
 
 		try {
 			join();
-			//findFile(this.name, this.type, this.date, this.size, this.content, this.path);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static void findFile(
-			String name, 
-			String type, 
-			String date, 
-			int size, 
-			String content, 
-			String path)
-			throws IOException
-	{
+	void findFile(String type, String name, int size, String path) throws IOException {
+
 		File folder = new File(path);
+
 		for (File file : folder.listFiles()) {
-			if (!file.isDirectory()) {
-
-				String fileContent = readFile(file);
-				String fileTime = getFileDate(file);				
-
-				if (
-						file.getName().contains(type) && 
-						file.getName().contains(name) && 
-						file.length() == size && 
-						fileContent.contains(content) &&
-						fileTime.contains(date)
-						) 
-				{
-					System.out.println("Arquivo encontrado: " + file.getName());
-				} else {
-					//System.out.println("Arquivo não encontrado");
-				}
-				
+			System.out.println();
+			if (file.isDirectory()) {
+				String newPath = path + "/" + file.getName();
+				Finder teste = new Finder(type, name, size, newPath);
 			} else {
-				//System.out.println("Entrou na pasta: " + file.getName());
-				String newPath = "/home/lucas/Documents/Prova/" + file.getName();
-				new Finder(name, type, date, size, content, newPath);
+				if (file.getName().contains(type) && file.getName().contains(name) && file.length() == size) {
+					System.out.println("Achei o arquivo " + file.getName() + " na " + atual.getName()
+							+ " dentro da pasta /" + file.getParentFile().getName());
+					System.exit(0);
+				} else {
+					System.out.println("Not a match on " + atual.getName());
+				}
 			}
 		}
 	}
 
-	public static String readFile(File file) throws IOException {
+	String readFile(File file) throws IOException {
 		String result = Files.readString(Paths.get(file.getPath()));
 		return result;
 	}
-	
-	public static String getFileDate(File file) throws IOException {
+
+	String getFileDate(File file) throws IOException {
 		Path filePath = Paths.get(file.getPath());
-		
-		BasicFileAttributes fileAttributes =
-                Files.readAttributes(filePath, BasicFileAttributes.class);
-		
+
+		BasicFileAttributes fileAttributes = Files.readAttributes(filePath, BasicFileAttributes.class);
+
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(new Date(fileAttributes.creationTime().to(TimeUnit.MILLISECONDS)));
-		
+
 		String finalDay = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
 		String finalMonth = String.valueOf(calendar.get(Calendar.MONTH) + 1);
 		String finalYear = String.valueOf(calendar.get(Calendar.YEAR));
-		
+
 		String finalDate = finalDay + "/" + finalMonth + "/" + finalYear;
-		
+
 		return finalDate;
 	}
-	
-	@Override
-    public void run() {
-        System.out.println("Nome da thread: " + atual.getName());
-        try {
-			findFile(this.name, this.type, this.date, this.size, this.content, this.path);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    }
 }
