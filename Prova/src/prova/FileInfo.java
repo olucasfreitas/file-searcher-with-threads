19df7a180167790e168b554c93c548c3ac8ab223
Lucas da SIlva Freitas <lucasdasilvafreitas090@gmail.com>
2022-04-04 23:09:38 -0300

    Create FileInfo.java

---
+++ Prova/src/prova/FileInfo.java
@@ -0,0 +1,14 @@
+package prova;
+
+public class FileInfo {
+	String fileName;
+	String threadName;
+	String parentName;
+
+	public FileInfo(String fileName, String threadName, String parentName) {
+		this.fileName = fileName;
+		this.threadName = threadName;
+		this.parentName = parentName;
+	}
+
+}
